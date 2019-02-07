95eb562e70e0db8478849fe4ad22dd4a2eafbbff
zhangpei <zhangpe0312@qq.com>
2019-02-07 14:07:41 +0800

    提交权限模块自定义权限表达式使用

---
+++ lovesecurity/authorize/src/main/java/org/nix/love/security/rbac/RbacService.java
@@ -0,0 +1,24 @@
+package org.nix.love.security.rbac;
+
+import org.springframework.security.core.Authentication;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author zhangpei
+ * @version 1.0
+ * @description 权限配置接口
+ * @date 2019/2/7
+ */
+public interface RbacService {
+    /**
+     * @param request 用户请求
+     * @param authentication 用户信息
+     * @return boolean
+     * @description 判断用户是否有某个权限没有
+     * @author zhangpe0312@qq.com
+     * @date 2019/2/7
+     */
+    boolean hasPermission(HttpServletRequest request, Authentication authentication);
+
+}
---
+++ lovesecurity/authorize/src/main/java/org/nix/love/security/rbac/RbacServiceImpl.java
@@ -0,0 +1,44 @@
+package org.nix.love.security.rbac;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import org.springframework.util.AntPathMatcher;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author zhangpei
+ * @version 1.0
+ * @description rbac权限服务具体实现
+ * @date 2019/2/7
+ */
+@Component
+public class RbacServiceImpl implements RbacService {
+
+    /**
+     * 路径匹配工具
+     */
+    private AntPathMatcher antPathMatcher = new AntPathMatcher();
+
+    @Override
+    public boolean hasPermission(HttpServletRequest request, Authentication authentication) {
+        Object principal = authentication.getPrincipal();
+        if (principal instanceof UserDetails) {
+            UserDetails userDetails = (UserDetails) principal;
+            String username = userDetails.getUsername();
+            // 读取用户所有用的所有url
+            Set<String> urls = new HashSet<>();
+            // 当前请求的uri
+            String requestURI = request.getRequestURI();
+            for (String url : urls) {
+                if (antPathMatcher.match(url,requestURI)){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
