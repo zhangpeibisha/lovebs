ab3fd0495129cd55939c9a24f3ba624361cc87d5
zhangpei <zhangpe0312@qq.com>
2019-04-20 01:01:07 +0800

    删除hello world接口

--- evaluation/src/main/java/org/nix/lovedomain/web/controller/UserController.java
+++ evaluation/src/main/java/org/nix/lovedomain/web/controller/UserController.java
@@ -1,7 +1,5 @@
 package org.nix.lovedomain.web.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -12,11 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class UserController {
-
-    @GetMapping(value = "/hello/{id}")
-    public String hello(@PathVariable Integer id) {
-        return "hello " + id;
-    }
 
 
 }
