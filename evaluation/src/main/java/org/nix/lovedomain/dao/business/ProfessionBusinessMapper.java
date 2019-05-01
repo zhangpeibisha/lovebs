02ad7d88bd5afb593d7c224ed3ec515788060030
zhangpei <zhangpe0312@qq.com>
2019-05-01 18:51:19 +0800

    提交通用mapper的使用

---
+++ evaluation/src/main/java/org/nix/lovedomain/dao/business/ProfessionBusinessMapper.java
@@ -0,0 +1,14 @@
+package org.nix.lovedomain.dao.business;
+
+import org.nix.lovedomain.dao.base.BaseBusinessMapper;
+import org.nix.lovedomain.dao.model.Profession;
+
+/**
+ * @author zhangpei
+ * @version 1.0
+ * @description 专业业务类
+ * @date 2019/5/1
+ */
+public interface ProfessionBusinessMapper extends BaseBusinessMapper<Profession> {
+
+}
--- evaluation/src/main/java/org/nix/lovedomain/service/ClassService.java
+++ evaluation/src/main/java/org/nix/lovedomain/service/ClassService.java
@@ -1,7 +1,14 @@
 package org.nix.lovedomain.service;
 
+import org.nix.lovedomain.dao.business.ClassBusinessMapper;
+import org.nix.lovedomain.model.Class;
 import org.nix.lovedomain.service.base.BaseService;
+import org.nix.lovedomain.service.vo.ClassVo;
+import org.nix.lovedomain.service.vo.PageVo;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * @version 1.0
@@ -10,4 +17,10 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class ClassService extends BaseService {
+
+    @Resource
+    private ClassBusinessMapper classBusinessMapper;
+
+
+
 }
