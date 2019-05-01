a2e2ac89c956f100e428fad318fd69e34e514b61
zhangpei <zhangpe0312@qq.com>
2019-05-01 20:56:01 +0800

    提交学院和班级查询接口

--- evaluation/src/main/java/org/nix/lovedomain/service/ClassService.java
+++ evaluation/src/main/java/org/nix/lovedomain/service/ClassService.java
@@ -1,7 +1,16 @@
 package org.nix.lovedomain.service;
 
+import org.nix.lovedomain.dao.business.ClassBusinessMapper;
+import org.nix.lovedomain.dao.business.FacultyBusinessMapper;
+import org.nix.lovedomain.model.Class;
+import org.nix.lovedomain.model.Faculty;
 import org.nix.lovedomain.service.base.BaseService;
+import org.nix.lovedomain.service.vo.PageVo;
+import org.nix.lovedomain.utils.SQLUtil;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * @version 1.0
@@ -11,7 +20,25 @@ import org.springframework.stereotype.Service;
 @Service
 public class ClassService extends BaseService {
 
+    @Resource
+    private ClassBusinessMapper classBusinessMapper;
 
+
+    public PageVo<Class> findClassPage(Integer page,
+                                       Integer limit,
+                                       String sql) {
+        page = SQLUtil.getOffset(page, limit);
+        List<Class> studentBySql
+                = classBusinessMapper.findClassBySql(page, limit, sql);
+        Long aLong = classBusinessMapper.countClassBySql(sql);
+        return PageVo.<Class>builder()
+                .page(page)
+                .limit(limit)
+                .total(aLong)
+                .data(studentBySql)
+                .build();
+
+    }
 
 
 
