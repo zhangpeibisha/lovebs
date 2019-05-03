20db110f35996a934c79d661703f50a866a5ecfa
zhangpei <zhangpe0312@qq.com>
2019-05-04 00:07:34 +0800

    修改参数名字，更标准的评卷id

--- evaluation/src/main/java/org/nix/lovedomain/web/controller/EvaluationquestionnaireController.java
+++ evaluation/src/main/java/org/nix/lovedomain/web/controller/EvaluationquestionnaireController.java
@@ -57,47 +57,47 @@ public class EvaluationquestionnaireController extends BaseController<Evaluation
 
     @ApiOperation(value = "一次性添加多个问题", notes = "提供一个问卷id，为该问卷添加多个问题内容")
     @PostMapping(value = "/batch/question")
-    public RespondsMessage addQuestion(@RequestParam(value = "questionId") Integer questionId,
+    public RespondsMessage addQuestion(@RequestParam(value = "evaluationId ") Integer evaluationId,
                                        @RequestBody List<BaseQuestion> question,
                                        Principal principal) {
         Evaluationquestionnaire evaluationquestionnaire
-                = evaluationquestionnaireService.addQuestion(questionId, question, principal);
+                = evaluationquestionnaireService.addQuestion(evaluationId, question, principal);
         if (evaluationquestionnaire != null) {
             return RespondsMessage.success(LogUtil
                     .logInfo(log, "用户{}在问卷【{}】中添加问题成功", principal.getName(),
                             evaluationquestionnaire.getTitle()), evaluationquestionnaire);
         }
-        throw new ServiceException(StrUtil.format("请检查问卷{}是否存在", questionId));
+        throw new ServiceException(StrUtil.format("请检查问卷{}是否存在", evaluationId));
     }
 
     @ApiOperation(value = "更新一个问题", notes = "需要提供更新的问卷id和问题更新后的信息，id和旧问题保持一致")
     @PutMapping(value = "/question")
-    public RespondsMessage updateQuestion(@RequestParam(value = "questionId") Integer questionId,
+    public RespondsMessage updateQuestion(@RequestParam(value = "evaluationId") Integer evaluationId,
                                           @RequestBody BaseQuestion<? extends BaseItem> question,
                                           Principal principal) {
         Evaluationquestionnaire evaluationquestionnaire
-                = evaluationquestionnaireService.updateQuestionItem(questionId, question, principal);
+                = evaluationquestionnaireService.updateQuestionItem(evaluationId, question, principal);
         if (evaluationquestionnaire != null) {
             return RespondsMessage.success(LogUtil
                     .logInfo(log, "用户{}在问卷【{}】中更新问题{}成功", principal.getName(),
                             evaluationquestionnaire.getTitle(), question.getId()), evaluationquestionnaire);
         }
-        throw new ServiceException(StrUtil.format("请检查问卷{}是否存在", questionId));
+        throw new ServiceException(StrUtil.format("请检查问卷{}是否存在", evaluationId));
     }
 
     @ApiOperation(value = "删除一个问题", notes = "需要提供删除的问卷id和问题id信息的信息")
     @DeleteMapping(value = "/question")
-    public RespondsMessage deleteQuestion(@RequestParam(value = "questionId") Integer questionId,
+    public RespondsMessage deleteQuestion(@RequestParam(value = "evaluationId") Integer evaluationId,
                                           @RequestBody BaseQuestion<? extends BaseItem> question,
                                           Principal principal) {
         Evaluationquestionnaire evaluationquestionnaire
-                = evaluationquestionnaireService.deleteQuestionItem(questionId, question, principal);
+                = evaluationquestionnaireService.deleteQuestionItem(evaluationId, question, principal);
         if (evaluationquestionnaire != null) {
             return RespondsMessage.success(LogUtil
                     .logInfo(log, "用户{}在问卷【{}】中删除问题{}成功", principal.getName(),
                             evaluationquestionnaire.getTitle(), question.getId()), evaluationquestionnaire);
         }
-        throw new ServiceException(LogUtil.logInfo(log, "请检查问卷{}是否存在", questionId));
+        throw new ServiceException(LogUtil.logInfo(log, "请检查问卷{}是否存在", evaluationId));
     }
 
     @ApiOperation(value = "查询用户自己拥有的问卷列表", notes = "用户登陆后，且拥有权限时可以查看自己的所有创建的问卷")
