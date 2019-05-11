328708ef4073a8c3919b2c46e3543fc081019d40
zhangpei <zhangpe0312@qq.com>
2019-05-11 18:55:35 +0800

    提交课程查询方法

---
+++ evaluation/src/main/java/org/nix/lovedomain/dao/business/CoueseBusinessMapper.java
@@ -0,0 +1,36 @@
+package org.nix.lovedomain.dao.business;
+
+import org.apache.ibatis.annotations.Param;
+import org.nix.lovedomain.model.Course;
+import org.nix.lovedomain.model.Teacher;
+
+import java.util.List;
+
+/**
+ * @author zhangpei
+ * @version 1.0
+ * @description 课程业务
+ * @date 2019/5/11
+ */
+public interface CoueseBusinessMapper {
+
+    /**
+     * 通过sql查询课程信息
+     *
+     * @param page
+     * @param limit
+     * @param sql
+     * @return
+     */
+    List<Course> findCourseBySql(@Param(value = "page") Integer page,
+                                 @Param(value = "limit") Integer limit,
+                                 @Param(value = "sql") String sql);
+
+    /**
+     * 通过sql查询老师数量
+     * @param sql
+     * @return
+     */
+    Long countCourseBySql(@Param(value = "sql") String sql);
+
+}
--- evaluation/src/main/java/org/nix/lovedomain/web/controller/CourseController.java
+++ evaluation/src/main/java/org/nix/lovedomain/web/controller/CourseController.java
@@ -2,27 +2,33 @@ package org.nix.lovedomain.web.controller;
 
 import org.nix.lovedomain.model.Course;
 import org.nix.lovedomain.service.CourseService;
+import org.nix.lovedomain.service.vo.PageVo;
 import org.nix.lovedomain.web.controller.base.BaseController;
+import org.nix.lovedomain.web.controller.dto.RespondsMessage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
 
 /**
  * @version 1.0
  * @anthor on 2019/4/19
  * @since jdk8
  */
-@Controller
+@RestController
 @RequestMapping(value = "course")
 public class CourseController extends BaseController<Course> {
 
     @Autowired
     CourseService courseService;
-    @RequestMapping(value = "test",method = RequestMethod.GET)
-    @ResponseBody
-    public int testList(){
-     return   courseService.testList();
+
+    @GetMapping(value = "/quire/list")
+    public RespondsMessage findTeacherPage(@RequestParam(value = "page", defaultValue = "1") Integer page,
+                                           @RequestParam(value = "limit", defaultValue = "10") Integer limit,
+                                           @RequestParam(value = "quire", required = false) String sql) throws IOException {
+        PageVo<Course> courseList = courseService.findCourseList(page, limit, sql);
+        return RespondsMessage.success("获取课程列表成功",courseList);
     }
+
 }
