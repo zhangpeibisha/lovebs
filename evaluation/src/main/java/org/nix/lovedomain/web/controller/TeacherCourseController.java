501dc80b6e918758801ba42fa8f3dd42c4ece1eb
zhangpei <zhangpe0312@qq.com>
2019-05-12 01:35:26 +0800

    完成课程的删除工作

--- evaluation/src/main/java/org/nix/lovedomain/web/controller/TeacherCourseController.java
+++ evaluation/src/main/java/org/nix/lovedomain/web/controller/TeacherCourseController.java
@@ -4,13 +4,18 @@ import org.nix.lovedomain.model.TeacherCourse;
 import org.nix.lovedomain.web.controller.base.BaseController;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @version 1.0
  * @anthor on 2019/4/19
  * @since jdk8
  */
-@Controller
+@RestController
 @RequestMapping(value = "teacherCourse")
 public class TeacherCourseController extends BaseController<TeacherCourse> {
+
+
+
+
 }
