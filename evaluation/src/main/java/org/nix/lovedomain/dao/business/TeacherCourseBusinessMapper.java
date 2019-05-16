68b5c68609047e92d4dd7bc6684858ff6c44d051
zhangpei <zhangpe0312@qq.com>
2019-05-17 02:47:39 +0800

    发布问卷完成

--- evaluation/src/main/java/org/nix/lovedomain/dao/business/TeacherCourseBusinessMapper.java
+++ evaluation/src/main/java/org/nix/lovedomain/dao/business/TeacherCourseBusinessMapper.java
@@ -1,7 +1,11 @@
 package org.nix.lovedomain.dao.business;
 
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 import org.nix.lovedomain.dao.base.BaseBusinessMapper;
 import org.nix.lovedomain.dao.model.TeacherCourseModel;
+
+import java.util.List;
 
 /**
  * @author zhangpei
@@ -10,4 +14,19 @@ import org.nix.lovedomain.dao.model.TeacherCourseModel;
  * @date 2019/5/12
  */
 public interface TeacherCourseBusinessMapper extends BaseBusinessMapper<TeacherCourseModel> {
+
+    /**
+     * 查询指定课程在指定时间老师的授课情况
+     *
+     * @param courseId  课程id集合
+     * @param startTime 开始搜索时间
+     * @param endTime   开始结束时间
+     * @return 老师+课程定位老师的授课课程
+     */
+    List<TeacherCourseModel> findTeacherCourseInCurrSemester(
+            @Param(value = "courseId") List<Integer> courseId,
+            @Param(value = "startTime") String startTime,
+            @Param(value = "endTime") String endTime
+    );
+
 }
--- evaluation/src/main/java/org/nix/lovedomain/web/controller/PublishquestionnaireController.java
+++ evaluation/src/main/java/org/nix/lovedomain/web/controller/PublishquestionnaireController.java
@@ -1,6 +1,10 @@
 package org.nix.lovedomain.web.controller;
 
 import cn.hutool.core.collection.CollUtil;
+import cn.hutool.core.date.DateTime;
+import cn.hutool.core.date.DateUnit;
+import cn.hutool.core.date.DateUtil;
+import cn.hutool.core.util.StrUtil;
 import com.alibaba.fastjson.JSON;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -8,11 +12,15 @@ import io.swagger.annotations.ApiParam;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.nix.lovedomain.dao.business.PublishQuestionBusinessMapper;
+import org.nix.lovedomain.dao.business.TeacherBusinessMapper;
+import org.nix.lovedomain.dao.business.TeacherCourseBusinessMapper;
 import org.nix.lovedomain.dao.business.json.winding.PublishAttachInfo;
 import org.nix.lovedomain.dao.mapper.EvaluationquestionnaireMapper;
 import org.nix.lovedomain.dao.model.PublishquestionnaireModel;
+import org.nix.lovedomain.dao.model.TeacherCourseModel;
 import org.nix.lovedomain.model.Evaluationquestionnaire;
 import org.nix.lovedomain.model.Publishquestionnaire;
+import org.nix.lovedomain.model.TeacherCourse;
 import org.nix.lovedomain.service.AccountService;
 import org.nix.lovedomain.service.PublishquestionnaireService;
 import org.nix.lovedomain.service.ServiceException;
@@ -29,6 +37,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.annotation.Resource;
 import java.security.Principal;
 import java.util.List;
+import java.util.function.Consumer;
 
 /**
  * @version 1.0
@@ -45,18 +54,29 @@ public class PublishquestionnaireController extends BaseController<Publishquesti
     private PublishquestionnaireService publishquestionnaireService;
 
     @Resource
-    public PublishQuestionBusinessMapper publishQuestionBusinessMapper;
-
-    @Resource
     public EvaluationquestionnaireMapper evaluationquestionnaireMapper;
 
     @Resource
     private AccountService accountService;
 
+    /**
+     * 发布问卷
+     *
+     * @param principal          登陆信息
+     * @param courseAndTeacherId 老师+课程的最新一条数据id
+     * @param teacherId          老师id
+     * @param questionnaireId    问卷id
+     * @param description        发布描述
+     * @param startRespondTime   开始回答时间
+     * @param endRespondTime     结束回答时间
+     * @param blacks             黑名单个数
+     * @return 响应信息
+     * @throws Exception 异常信息
+     */
     @ApiOperation(value = "发布评教问卷")
     @PostMapping(value = "/publish")
     public RespondsMessage pusblishQuestionnaire(Principal principal,
-                                                 @RequestParam(value = "courseId") Integer courseId,
+                                                 @RequestParam(value = "courseAndTeacherId") Integer courseAndTeacherId,
                                                  @RequestParam(value = "teacherId") Integer teacherId,
                                                  @RequestParam(value = "questionnaireId") Integer questionnaireId,
                                                  @RequestParam(value = "description", required = false) String description,
@@ -69,11 +89,132 @@ public class PublishquestionnaireController extends BaseController<Publishquesti
             throw new ServiceException("用户未登陆");
         }
         Publishquestionnaire publishquestionnaire = publishquestionnaireService.pusblishQuestionnaire(principal,
-                courseId, teacherId, questionnaireId, description, startRespondTime, endRespondTime, blacks);
+                courseAndTeacherId, teacherId, questionnaireId, description, startRespondTime, endRespondTime, blacks);
 
         return RespondsMessage.success(LogUtil.logInfo(log, "{}发布评教问卷成功", principal.getName()),
                 publishquestionnaire);
     }
+
+    /**
+     * 根据课程id批量发布问卷
+     *
+     * @param principal        用户登陆信息
+     * @param courseIds        课程id集合
+     * @param questionnaireId  问卷id
+     * @param description      发布描述
+     * @param startRespondTime 开始答卷时间
+     * @param endRespondTime   结束答卷时间
+     * @param blacks           黑名单数量
+     * @return 响应信息
+     * @throws Exception 抛出异常
+     */
+    @ApiOperation(value = "发布评教问卷")
+    @PostMapping(value = "/publish/by/courseIds")
+    public RespondsMessage publishQuestionnaireByCourseIds(Principal principal,
+                                                            @RequestParam(value = "courseIds") List<Integer> courseIds,
+                                                            @RequestParam(value = "questionnaireId") Integer questionnaireId,
+                                                            @RequestParam(value = "description", required = false) String description,
+                                                            @RequestParam(value = "startRespondTime") Long startRespondTime,
+                                                            @RequestParam(value = "endRespondTime") Long endRespondTime,
+                                                            @ApiParam(value = "发布人可以设置该任课老师可以屏蔽几个学生的成绩不计入统计")
+                                                            @RequestParam(value = "blacks", defaultValue = "5") Integer blacks) throws Exception {
+
+        if (principal == null) {
+            throw new ServiceException("用户未登陆");
+        }
+        List<TeacherCourseModel> teacherCourseInCurrSemester = findTeacherCourseInCurrSemester(courseIds);
+        if (CollUtil.isEmpty(teacherCourseInCurrSemester)) {
+            return RespondsMessage.failure(LogUtil.logInfo(log,
+                    "发布失败，没用找到课程{}的授课老师,在当前学期", courseIds));
+        }
+        teacherCourseInCurrSemester.forEach(teacherCourseModel -> {
+            try {
+                Integer teacherAccountId = teacherCourseModel.getTeacherId();
+                publishquestionnaireService.pusblishQuestionnaire(principal,
+                        teacherCourseModel.getId(), teacherAccountId,
+                        questionnaireId, description,
+                        startRespondTime,
+                        endRespondTime, blacks);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+        return RespondsMessage.success(StrUtil.format("发布针对课程{}的问卷成功", courseIds));
+    }
+
+    @Resource
+    private TeacherCourseBusinessMapper teacherCourseBusinessMapper;
+
+    /**
+     * 通过course表中的课程id获取到所有当前学期参与授课的老师
+     *
+     * @param courseId 课程id集合
+     * @return 老师课程信息
+     */
+    public List<TeacherCourseModel> findTeacherCourseInCurrSemester(List<Integer> courseId) {
+        if (CollUtil.isEmpty(courseId)) {
+            return null;
+        }
+        String currSemesterStartTime = findCurrSemesterStartTime();
+        String currSemesterEndTime = findCurrSemesterEndTime();
+        return teacherCourseBusinessMapper.findTeacherCourseInCurrSemester(courseId,
+                currSemesterStartTime, currSemesterEndTime);
+    }
+
+    /**
+     * 当前月份大于等于9，则认为是第一续期
+     */
+    private int september = 9;
+    /**
+     * 当前月份小于3，认为是第一学期
+     */
+    private int march = 3;
+
+
+    /**
+     * 获取当前学期的开始时间
+     *
+     * @return 当前学期的开始时间
+     */
+    public String findCurrSemesterStartTime() {
+        DateTime date = DateUtil.date();
+        int year = date.year();
+        int month = date.month();
+        // 第一学期
+        if (month >= september) {
+            return getTime(year, september);
+        }
+        if (month < march) {
+            return getTime(year - 1, september);
+        }
+        // 第二学期
+        return getTime(year, march);
+    }
+
+    /**
+     * 获取当前学期的结束时间
+     *
+     * @return 当前学期的结束时间
+     */
+    public String findCurrSemesterEndTime() {
+        DateTime date = DateUtil.date();
+        int year = date.year();
+        int month = date.month();
+        // 第一学期
+        if (month >= september) {
+            return getTime(year + 1, march);
+        }
+        if (month < march) {
+            return getTime(year, march);
+        }
+        // 第二学期
+        return getTime(year, september);
+    }
+
+    public String getTime(int year, int month) {
+        return StrUtil.format("{}-{}-{} 00:00:00", year, month, 1);
+    }
+
 
     /**
      * 添加黑名单学生
@@ -243,16 +384,15 @@ public class PublishquestionnaireController extends BaseController<Publishquesti
 
     /**
      * 查看发布问卷的评分
+     *
      * @param publishId
      * @return
      */
     @GetMapping(value = "/score")
-    public RespondsMessage teacherViewStat(@RequestParam(value = "publishId") Integer publishId){
+    public RespondsMessage teacherViewStat(@RequestParam(value = "publishId") Integer publishId) {
         return RespondsMessage.success("获取统计结果完成",
                 publishquestionnaireService.getQuestionStatisticalScore(publishId));
     }
-
-
 
 
 }
